58119c951a21a3b43878e8e6458fc78317b90007
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-08-22 17:51:32 +0200

    Increment cache version in service worker
    Updated cache version to v10.6.72.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gasdrive-v10.6.71'; // SUBE ESTA VERSION CADA VEZ QUE CAMBIES ALGO
+const CACHE = 'gasdrive-v10.6.72'; // SUBE ESTA VERSION CADA VEZ QUE CAMBIES ALGO
 const FILES = [
   './',
   './index.html',
