763f5c6a9a4672c8a307945ac8b929dc37dacdd5
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-08-24 16:38:29 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gasdrive-v10.6.77'; // SUBE ESTA VERSION CADA VEZ QUE CAMBIES ALGO
+const CACHE = 'gasdrive-v10.6.78'; // SUBE ESTA VERSION CADA VEZ QUE CAMBIES ALGO
 const FILES = [
   './',
   './index.html',
