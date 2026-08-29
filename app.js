35417abd1c7c1d8a86ad8f26b5c6d1462d1e484e
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-08-29 15:41:32 +0200

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // GASDRIVE DGT V8.14 CAT - 630 PREGUNTES DGT 2026
-const VERSION = "9.8.9";
+const VERSION = "12.0";
 
 // COMBO DOPAMINA
 const EMOJIS_ENCERT = ['🚀','💎','👑','🔥','💯','⚡','🏆','🦄','🤑','✅','💪','😎','🎯','💥','🌟','🎉'];
